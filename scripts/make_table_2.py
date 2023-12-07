25204b9ec8bfa32a2225d81e6b6f7b2de8dd9c09
Aaron Mishkin <aaronpmishkin@gmail.com>
2023-12-07 09:51:50 -0800

    Add XGBoost as a baseline.

--- scripts/make_table_2.py
+++ scripts/make_table_2.py
@@ -7,6 +7,7 @@ import os
 from sklearn.ensemble import RandomForestClassifier  # type: ignore
 from sklearn.svm import SVC  # type: ignore
 import numpy as np
+import xgboost as xgb
 
 from scaffold import datasets
 from scaffold.uci_names import BINARY_SMALL_UCI_DATASETS
@@ -94,7 +95,7 @@ seed = 650
 verbose = False
 
 
-def cv_experiment(classifier, hyperparameters, dataset_name):
+def cv_experiment(name, classifier, hyperparameters, dataset_name):
     uci_data = {
         "name": dataset_name,
         "split_seed": 1995,
@@ -113,15 +114,22 @@ def cv_experiment(classifier, hyperparameters, dataset_name):
             train, valid = load_data(data_config)
 
             clf = classifier(random_state=seed, **params)
-            clf.fit(train[0], train[1])
+            X_train, y_train = train
+            X_valid, y_valid = valid
 
-            validation_accuracy = accuracy(clf.predict(valid[0]), valid[1])
+            if name == "xg_boost":
+                y_train[y_train == -1] = 0
+                y_valid[y_valid == -1] = 0
+
+            clf.fit(X_train, y_train)
+
+            validation_accuracy = accuracy(clf.predict(X_valid), y_valid)
 
             if verbose:
                 print(params)
                 print(
                     "Train Accuracy:",
-                    accuracy(clf.predict(train[0]), train[1]),
+                    accuracy(clf.predict(X_train), y_train),
                 )
                 print("Valid Accuracy:", validation_accuracy)
 
@@ -137,12 +145,18 @@ def cv_experiment(classifier, hyperparameters, dataset_name):
         "use_valid": False,
     }
     train, test = load_data(test_config)
+    X_train, y_train = train
+    X_test, y_test = test
+
+    if name == "xg_boost":
+        y_train[y_train == -1] = 0
+        y_test[y_test == -1] = 0
 
     clf = classifier(random_state=seed, **best_params)
-    clf.fit(train[0], train[1])
+    clf.fit(X_train, y_train)
 
-    train_acc = accuracy(clf.predict(train[0]), train[1])
-    test_acc = accuracy(clf.predict(test[0]), test[1])
+    train_acc = accuracy(clf.predict(X_train), y_train)
+    test_acc = accuracy(clf.predict(X_test), y_test)
 
     return best_params, train_acc, test_acc
 
@@ -171,36 +185,66 @@ kernel_svm_params = [
     for lam, gamma in product(lambda_to_try, bandwidths)
 ]
 
+depths = [2, 4, 6, 10]
+sizes = [5, 10, 100, 1000, 5000]
+xgb_params = [
+    {"n_estimators": size, "max_depth": depth}
+    for size, depth in product(sizes, depths)
+]
+
+methods = ["random_forest", "xg_boost", "linear_svm", "kernel_svm"]
+method_classes = [RandomForestClassifier, xgb.XGBClassifier, SVC, SVC]
+method_params_list = [
+    rf_params,
+    xgb_params,
+    linear_svm_params,
+    linear_svm_params,
+    kernel_svm_params,
+]
+
+learning_algos = list(zip(methods, method_classes, method_params_list))
+
 dataset_list = BINARY_SMALL_UCI_DATASETS
 
+force = False
+
 for dataset_name in dataset_list:
-    # for dataset_name in ["car"]:
-    print(f"Running dataset {dataset_name}.")
+    print(dataset_name)
+    all_results[dataset_name] = []
+    for name, method_class, method_params in learning_algos:
+        # for dataset_name in ["car"]:
+        print(f"Running dataset {dataset_name}.")
+        if (
+            os.path.exists(
+                f"results/binary_baselines/{dataset_name}_{name}.pkl"
+            )
+            and not force
+        ):
+            print("Loading results...")
 
-    if os.path.exists(f"results/binary_baselines/{dataset_name}.pkl"):
-        print("Loading results...")
+            with open(
+                f"results/binary_baselines/{dataset_name}_{name}.pkl", "rb"
+            ) as f:
+                res = pkl.load(f)
+        else:
+            print(f"Running {name}...")
 
-        with open(f"results/binary_baselines/{dataset_name}.pkl", "rb") as f:
-            res = pkl.load(f)
-    else:
-        print("Running RF")
-        baseline_res = cv_experiment(
-            RandomForestClassifier, rf_params, dataset_name
-        )
-        print("Running L-SVM")
-        linear_svm_res = cv_experiment(SVC, linear_svm_params, dataset_name)
-        print("Running K-SVM")
-        kernel_svm_res = cv_experiment(SVC, kernel_svm_params, dataset_name)
+            res = cv_experiment(
+                name,
+                method_class,
+                method_params,
+                dataset_name,
+            )
 
-        res = (baseline_res, linear_svm_res, kernel_svm_res)
+            # save result
+            with open(
+                f"results/binary_baselines/{dataset_name}_{name}.pkl", "wb"
+            ) as f:
+                pkl.dump(res, f)
 
-        # save result
-        with open(f"results/binary_baselines/{dataset_name}.pkl", "wb") as f:
-            pkl.dump(res, f)
+        all_results[dataset_name].append(res)
 
-    all_results[dataset_name] = res
-
-result_str = "Dataset, C-GReLU, C-ReLU, Deep C-GReLU, Random Forest, Linear SVM, Kernel SVM \n"
+result_str = "Dataset, C-GReLU, C-ReLU, Deep C-GReLU, Random Forest, XGBoost, Linear SVM, Kernel SVM \n"
 
 result_keys = list(all_results.keys())
 result_keys.sort()
@@ -223,6 +267,7 @@ for key in result_keys:
     result_str = result_str + ", " + str(round(value[0][2] * 100, 1))
     result_str = result_str + ", " + str(round(value[1][2] * 100, 1))
     result_str = result_str + ", " + str(round(value[2][2] * 100, 1))
+    result_str = result_str + ", " + str(round(value[3][2] * 100, 1))
 
     result_str = result_str + "\n"
 
