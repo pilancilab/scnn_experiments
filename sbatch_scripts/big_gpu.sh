49ffa297184a3dc61070689905acb7f93bb02bf2
Aaron Mishkin <aaronpmishkin@gmail.com>
2025-03-26 16:58:50 -0700

    Update sbatch scripts use to venv.

--- sbatch_scripts/big_gpu.sh
+++ sbatch_scripts/big_gpu.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #
 #SBATCH --job-name=gpu
-#SBATCH --time=12:00:00
+#SBATCH --time=18:00:00
 #SBATCH --partition=gpu
 #SBATCH --ntasks=1
 #SBATCH --gpus-per-task=1
-#SBATCH --cpus-per-task=4
-#SBATCH --mem-per-cpu=8G
-#SBATCH --constraint=GPU_SKU:RTX_2080Ti
+#SBATCH --cpus-per-task=2
+#SBATCH --mem-per-gpu=24G
+#SBATCH --constraint="GPU_SKU:V100_PCIE"
 
 ml reset
 ml restore scnn
--- sbatch_scripts/pilanci.sh
+++ sbatch_scripts/pilanci.sh
@@ -9,5 +9,6 @@
 
 ml reset
 ml restore scnn
+source venv/bin/activate
 
 eval $JOB_STR
